bda2f4b12b752b56a5047384441cd736d30cf4a6
Jassem Gaaloul <jassemgaaloul123@gmail.com>
2021-03-18 06:50:13 +0100

    builded for deployment

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "client")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "client", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT, () => {
